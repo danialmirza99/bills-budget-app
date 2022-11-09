df9146b3ed0d504197f0c44428e6b5ecde48aeec
A.Klein <allenkleined@gmail.com>
2022-11-09 08:38:51 -0800

    fixed user model and added item model

---
+++ models/Item.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Item extends Model{}
+
+Item.init({
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    due_date:{
+        type: DataTypes.STRING,
+        allowNull: false,
+
+    },
+    amount:{
+        type: DataTypes.DECIMAL,
+        allowNull:false
+    },
+    cost_id:{
+        references: {
+            model: 'user',
+            key: 'id'
+          }
+    }
+
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'item',
+}
+)
+module.exports = Item;
