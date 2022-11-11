1778072ed1df49fa55447411311068675f64a0c3
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-11-11 13:16:10 -0800

    Fixed minor problem in budget part

--- public/js/update-budget.js
+++ public/js/update-budget.js
@@ -2,7 +2,7 @@
 const updateBudgetHandler = async (event) => {
     event.preventDefault();
 
-    const newBudget = documet.querySelector('#updateBudget').value.trim();
+    const newBudget = document.querySelector('#updateBudget').value.trim();
 
     if (newBudget) {
         const response = await fetch('/api/budget', {
