6e93fc373f6702a132b59912f9ce301278c52040
NG <nicholeguan2@gmail.com>
2022-11-12 17:27:51 -0800

    add button and functionality to view all current year spendings

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -177,4 +177,46 @@ router.get('/profile/month/:month', withAuth, async (req, res) => {
   }
 });
 
+router.get('/profile/year', withAuth, async (req, res) => {
+  try {
+    let today = new Date();
+    let month = today.getMonth() + 1;
+    let monthName = func.getMonthName(month);
+    let year = today.getFullYear();
+
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Budget }, { model: Item }],
+    });
+
+    const user = userData.get({ plain: true });
+    const items = user.items;
+    const dates = items.map((item) => item.due_date);
+
+    const indexArr = func.indexMatchYearMonth(func.arrDates(dates), year, month);
+    const indexArrYear = func.indexMatchYear(func.arrDates(dates), year);
+
+    const amounts = items.map((item) => item.amount);
+    const cost = func.useIndex(indexArr, amounts);
+    const yearItems = func.useIndex(indexArrYear, items);
+
+    let total = func.sum(cost);
+    let yrtotal = func.sum(amounts);
+
+    res.render('profile', {
+      ...user,
+      monthName,
+      year,
+      total,
+      yrtotal,
+      yearItems,
+      logged_in: true,
+      year_view: true
+    });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -5,6 +5,14 @@ const costMonthFormHandler = async (event) => {
     document.location.replace(`/profile/month/${month}`)
 };
 
+const viewYear = async (event) => {
+    document.location.replace(`/profile/year`)
+};
+
 document
     .querySelector('.profile-cost-month')
     .addEventListener('submit', costMonthFormHandler);
+
+document
+    .querySelector('#year-btn')
+    .addEventListener('click', viewYear);
